80a6c2ddff6024612803a75b0d6da9177d8ca066
sandesh <sandeshjadhav2002@gmail.com>
2024-02-01 10:59:42 +0530

    Zero sum solved

---
+++ Arrays/Zero_sum_subarray.cpp
@@ -0,0 +1,19 @@
+video_link = "https://www.youtube.com/watch?v=HJWiQP5Kl-Q"
+
+long long int findSubarray(vector<long long int> &arr, int n ){
+
+   unordered_map<long long, int> mp;
+
+   long long sum = 0;
+   long long ans = 0;
+   mp[0] = 1;
+
+   for(int i=0; i<n; i++){
+    sum+= arr[i];
+    if(mp.find(sum) != mp.end()){
+        ans+=mp[sum];
+    }
+    mp[sum]++;
+   }
+  return ans;
+}
